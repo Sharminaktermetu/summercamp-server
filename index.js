194eab080500e2ec53c8f32c9c8faa1f3d4ff1ab
Sharminaktermetu <sharminacc1@gmail.com>
2023-07-02 14:46:21 +0600

    cart delete

--- index.js
+++ index.js
@@ -46,7 +46,12 @@ async function run() {
         res.send(result)
     })
 
- 
+    app.delete('/cart/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result =await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
